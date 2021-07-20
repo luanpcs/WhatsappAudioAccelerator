fc4e4a2027d8605ddb0fa23c0ac051717c996352
luanpcs <luancsiqueira@hotmail.com>
2021-07-19 22:09:25 -0300

    src

---
+++ content.js
@@ -0,0 +1,25 @@
+const interval = setInterval(() => {
+    const header = document.querySelector("._1G3Wr")
+    if (header) {
+        console.log(header)
+        clearInterval(interval)
+
+
+        const button = document.createElement("button")
+        button.innerHTML = "2x"
+        button.classList.add("twoTimesButton")
+
+        button.addEventListener("click", () => {
+            const audios = document.querySelectorAll("audio")
+
+            for (const audio in audios)
+                audio.playbackRate = 2
+
+        })
+        header.appendChild(button)
+    }
+}, 1000);
+
+
+
+
