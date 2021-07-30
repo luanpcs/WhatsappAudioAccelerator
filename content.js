0293eaebec7e018b5182391d4b27bd9b3ad79144
Luan Siqueira <63127056+luanpcs@users.noreply.github.com>
2021-07-30 10:16:29 -0300

    Update content.js

--- content.js
+++ content.js
@@ -3,23 +3,16 @@ const interval = setInterval(() => {
     if (header) {
         console.log(header)
         clearInterval(interval)
-
-
+        
         const button = document.createElement("button")
         button.innerHTML = "2x"
         button.classList.add("twoTimesButton")
 
         button.addEventListener("click", () => {
             const audios = document.querySelectorAll("audio")
-
             for (const audio in audios)
                 audio.playbackRate = 2
-
         })
         header.appendChild(button)
     }
 }, 1000);
-
-
-
-
